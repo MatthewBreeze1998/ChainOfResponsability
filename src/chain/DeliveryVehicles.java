b9ba20f0ac96955ca40e8f369ad2c1648bfa8937
t7077260 <t7077260@linux42.scm.tees.ac.uk>
2018-10-23 14:54:34 +0100

    Ramp check

--- src/chain/DeliveryVehicles.java
+++ src/chain/DeliveryVehicles.java
@@ -15,11 +15,29 @@ public class DeliveryVehicles
     public String destination;
     public boolean isAvailable;
 
-    public DeliveryVehicles(VehicleType type) {
+    
+    public DeliveryVehicles(VehicleType type) 
+    {
         this.type = type;
         this.isAvailable = true;
         vehicles.add(this);
     }
     
+    public String checkRamp()
+    {
+       while(type.RAMP != null) 
+       {
+          
+           if(this.type.RAMP.)
+           {
+               
+           }
+       
+       }
+       return type.RAMP.toString();
+           
+           
+    }
+   
     
 }
