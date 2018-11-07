35b27df74421e6ad902baf7e402a27d6b35d05ae
Matty <Matty@192.168.1.15>
2018-11-07 11:09:05 +0000

    testing needs doing

--- src/chain/Fuel.java
+++ src/chain/Fuel.java
@@ -22,6 +22,10 @@ public class Fuel extends NextCheck
     
     }
 
+    public Fuel(FuelEnum fuelEnum) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
    
     
     
